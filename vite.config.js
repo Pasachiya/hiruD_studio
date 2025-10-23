b57dcf04ef98bcf217b581f7d60f7347a8afdac8
Pasachiya <pasachiya@gmail.com>
2025-10-23 22:49:44 +0530

    Update routing and configuration for Vite deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'happy-dom',
-  },
+  base: '/', // user/organization site (pasachiya.github.io). For project site use '/repo-name/'.
 })
